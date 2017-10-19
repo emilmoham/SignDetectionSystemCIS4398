4f32a6124d70e8fd6f094e0b1d5733a69f2d587e
Emil Mohammed <emilmoham@github.com>
2017-10-19 18:15:35 -0400

    Attempted to add thread support. Very Broken...

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,16 +1,68 @@
-#include "SignDetection.h"
+#include "headers/GeneralHeaders.h"
+#include "headers/FrameStructs.h"
+#include "headers/consumer.h"
+/*
+This test currently runs, but is super broken. There's some memory overread
+or over write somewhere.
+
+I believe that we will need to implement our own version of a queue because
+the current implementation is not extendable through a heap allocated
+object.
+*/
+
+
+#define NUM_THREADS 1
+
+struct buffer *init();
 
 using namespace std;
-//using namespace cv;
 
 int main(int argc, char **argv){
-    queue<struct FrameResult> q;
+    //Threaed information
+    pthread_t threads[NUM_THREADS];
+    int rc;
 
-    struct Frame frame_obj(10);
-    struct Region region_obj(1, 2, 3, 4);
-    struct FrameResult fr(frame_obj, region_obj);
+    //Lock and Queue container
+    struct buffer buff;
 
-    fr.print();
+    //Allocate space for and initalize buffer
+    //buff = &(init());
 
-    return 0;
+    //initalize semaphore
+    //sem_init(&(buff->mutex), 0, 1);
+    sem_init(&(buff.elements), 0, 0);
+    int current_elements;
+
+    //Create Consumer threads
+    for(int i = 0; i < NUM_THREADS; i++){
+        printf("main(): creating thread, %d\n", i);
+        rc = pthread_create(&threads[i], NULL, consume, (void *)&buff);
+
+        if(rc){
+            printf("Error: unable to create thread, %d\n", rc);
+            exit(-1);
+        }
+    }
+
+    //Add elements to queue of Frame struct
+    for(int i = 0; i < NUM_THREADS; i++){
+        //sem_getvalue(&(buff->elements), &current_elements);
+        printf("%d\n", current_elements);
+        struct Frame frame_obj(i);
+        struct Region region_obj(i, i+1, i+2, i+3);
+        struct FrameResult FrameResult_obj(frame_obj, region_obj);
+        (buff.q).push(FrameResult_obj);
+        sem_post(&(buff.elements));
+    }
+
+    pthread_exit(NULL);
+}
+
+struct buffer *init(){
+    struct buffer *b;
+    if((b=(struct buffer *)malloc(sizeof(struct buffer))) == NULL){
+        fprintf(stderr, "Error Initializing buffer\n");
+        return NULL;
+    }
+    return b;
 }
