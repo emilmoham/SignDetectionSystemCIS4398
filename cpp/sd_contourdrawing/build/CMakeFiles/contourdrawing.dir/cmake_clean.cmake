ddfe573b5e1b074de635829fb9f5c34cd1cc4f66
Ben Wilson <ben.wilson1337@gmail.com>
2017-11-04 16:14:11 -0400

    Initializing directories

---
+++ cpp/sd_contourdrawing/build/CMakeFiles/contourdrawing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/contourdrawing.dir/contourdrawing.cpp.o"
+  "contourdrawing.pdb"
+  "contourdrawing"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/contourdrawing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
