bcb9363b90af85a73f4961358511e1279c4f4102
Timothy Vaccarelli <tmvfroid@gmail.com>
2017-11-11 21:48:19 +0000

    Fix linker errors and template compiler errors from last commit

--- cpp/sd_common/SafeQueue.h
+++ cpp/sd_common/SafeQueue.h
@@ -14,32 +14,27 @@ template <class T>
 class SafeQueue
 {
 public:
-    typename std::deque<T>::const_reference const_reference;
-    typename std::deque<T>::const_iterator const_iterator;
-    typename std::deque<T>::size_type size_type;
-    typename std::lock_guard<std::mutex> lock_guard;
-    
     /// Default constructor
     SafeQueue() = default;
 
     /// Returns a const_reference to the first element in the queue
-    const_reference front() const
+    typename std::deque<T>::const_reference front() const
     {
-        lock_guard lock(m_mutex);
+        std::lock_guard<std::mutex> lock(m_mutex);
         return m_queue.front();
     }
     
     /// Returns a const_reference to the element at the given position in the queue
-    const_reference at(size_type pos) const
+    typename std::deque<T>::const_reference at(typename std::deque<T>::size_type pos) const
     {
-        lock_guard lock(m_mutex);
+        std::lock_guard<std::mutex> lock(m_mutex);
         return m_queue.at(pos);
     }
 
     /// Returns true if the queue contains the given item, false if else
     bool contains(const T &item) const noexcept
     {
-        lock_guard lock(m_mutex);
+        std::lock_guard<std::mutex> lock(m_mutex);
         for (auto it = m_queue.cbegin(); it != m_queue.cend(); ++it)
         {
             if (*it == item)
@@ -51,7 +46,7 @@ public:
     /// Erases the item from the queue, if it is in the queue
     void erase(const T &item)
     {
-        lock_guard lock(m_mutex);
+        std::lock_guard<std::mutex> lock(m_mutex);
         for (auto it = m_queue.cbegin(); it != m_queue.cend(); ++it)
         {
             if (*it == item)
@@ -66,14 +61,14 @@ public:
     /// Appends a copy of the given element to the back of the queue
     void enqueue(const T &item)
     {
-        lock_guard lock(m_mutex);
+        std::lock_guard<std::mutex> lock(m_mutex);
         m_queue.push_back(item);
     }
 
     /// Removes and returns the item at the front of the queue
     T dequeue()
     {
-        lock_guard lock(m_mutex);
+        std::lock_guard<std::mutex> lock(m_mutex);
         T item = std::move(m_queue.front());
         m_queue.pop_front();
         return item;
@@ -86,7 +81,7 @@ public:
     }
 
     /// Returns the number of elements in the queue
-    size_type size() const noexcept
+    typename std::deque<T>::size_type size() const noexcept
     {
         return m_queue.size();
     }
